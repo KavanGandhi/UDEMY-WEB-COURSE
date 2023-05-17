346527fcab75ab4f3f4842bf89eda6c05e8d5a62
KavanGandhi <kavangandhi1@gmail.com>
2023-05-17 23:13:10 +0530

    newsletter-signup remaining added

--- Newsletter-signup/app.js
+++ Newsletter-signup/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const request = require("request");
 const bodyParser = require("body-parser");
+const https = require("https");
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 // here when we will run our server then we will not get proper css because some of the css comes from bootstrap css
@@ -12,23 +12,61 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html");
 })
 
-// we want to post our data to external resource so we will can't do https.get but we gave to do
-// https.request to post our data
+// here mailchimp api accepts data in json form so we have to convert user entered data into json form
+// so first we have to make a javascript object for it
 
 app.post("/",function(req,res){
-    var fname = req.body.fname;
-    var lname = req.body.lname;
-    var email = req.body.eml;
+    const fname = req.body.fname;
+    const lname = req.body.lname;
+    const email = req.body.eml;
 
     // console.log(fname+" "+lname+" "+email);
-    
+    const data = {
+        members:[
+            {
+                email_address:email,
+                status:"subscribed",
+                merge_fields:{
+                    FNAME:fname,
+                    LNAME:lname    
+                }
+            }
+        ]
+    };
+    const jsonData = JSON.stringify(data);//converts js object into string i.e it will be converted to JSON
+
+    // we want to post our data to external resource so we will can't do https.get but we gave to do
+    // https.request to post our data
+    // here options is for what we want to do get or post or any other thing default is get
+    const url="https://us21.api.mailchimp.com/3.0/lists/ca84a10983";
+    // in mailchimp there are various lists in mailchimp where user can suscribe so we have to use list id which is ca84a...
+
+    const options={
+        method:"POST",
+        auth:"kavan:" //we use auth for a valid authentication request to check that request is valid
+    }
+
+    // to send the data entered by user we have to save our request inside a variable and then do request.write to send data to mailchimp sever
+    const request = https.request(url, options, function (response) {
+        // console.log(response.statusCode);
+        if(response.statusCode===200)
+        {
+            res.sendFile(__dirname + "/success.html");
+        }
+        else
+        {
+            res.sendFile(__dirname + "/failure.html");
+        }
+        response.on("data",function(data){
+            // console.log(JSON.parse(data));
+        })
+    })
+    // request.write(jsonData);
+    request.end();
 })
 app.listen(3000,function(){
     console.log("Server started on port 3000");
 })
-
-// api key
-// 223fea1033dcf591accabdc05cc8e244-us21
 
 // unique id
 // ca84a10983
